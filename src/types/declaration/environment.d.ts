a7248d484f358ce6bbdd77a06e906e8de15841eb
Pushkar952 <kumar.pus95@gmail.com>
2022-10-01 15:35:06 +0530

    :rocket: Inital project setup

---
+++ src/types/declaration/environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  // eslint-disable-next-line
+  interface ProcessEnv extends NodeJS.ProcessEnv {
+    NEXT_PUBLIC_API_URL: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+
+  }
+}
